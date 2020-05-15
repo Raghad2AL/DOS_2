d06ea67ecd38835d309fd2f47efceaa722fd9fd9
Raghad AL <40498780+Raghad2AL@users.noreply.github.com>
2020-05-15 16:26:27 +0300

    Clone for Order server

--- server3.java
+++ server3.java
@@ -6,9 +6,9 @@ public class server3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Gson gson = new Gson();
-		port(2222);
+		port(4444);
     	get("Buy/:id",( req,res)->{double ID = Double.parseDouble(req.params(":id"));
-    	String path = "/home/raghad-al3/Desktop/SharedFolders/Books2.xlsx";
+    	String path = "/home/raghad-al3/Desktop/SharedFolders/Books.xlsx";
     		return gson.toJson(ExceleDataReader.LookUpFromExcel(path, ID));			
     	});
 
