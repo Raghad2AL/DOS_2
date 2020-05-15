41d28cdf7799d3162e70ca37200e5b1fa6aad746
Raghad AL <40498780+Raghad2AL@users.noreply.github.com>
2020-05-15 17:33:48 +0300

    Clone for order server

--- Order_Server_Clone.java
+++ Order_Server_Clone.java
@@ -1,14 +1,14 @@
 import static spark.Spark.*;
 
 import com.google.gson.Gson;
-public class server3 {
+public class Order_Server_Clone {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Gson gson = new Gson();
 		port(4444);
     	get("Buy/:id",( req,res)->{double ID = Double.parseDouble(req.params(":id"));
-    	String path = "/home/raghad-al3/Desktop/SharedFolders/Books.xlsx";
+    	String path = "/home/raghad-al3/Desktop/SharedFolders/Books2.xlsx";
     		return gson.toJson(ExceleDataReader.LookUpFromExcel(path, ID));			
     	});
 
